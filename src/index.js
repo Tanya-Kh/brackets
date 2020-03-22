e8c7c3df9bd3c97e46ddf83083b9971515bd208d
Tanya Khreshchykava <tanya.khreshchykava@gmail.com>
2020-03-22 15:01:23 -0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var strArray = str.split('');
+
+    for (let i = 0; i < strArray.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+
+            if (strArray[i] === bracketsConfig[j][0]) {
+                if (strArray[i + 1] === bracketsConfig[j][1]) {
+                    delete strArray[i];
+                    delete strArray[i + 1];
+                    strArray = strArray.filter(x => x !== undefined);
+                    i = -1;
+                    j = -1;
+                }
+            }
+        }
+    }
+    if (strArray.length === 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
+
+
+
